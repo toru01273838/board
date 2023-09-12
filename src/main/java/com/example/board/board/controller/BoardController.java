aab70f42a144bb8d6e8f37c3415e6aa85c1a6bd8
toru <01273838honda@gmail.com>
2023-09-12 21:31:58 +0900

    commit

--- src/main/java/com/example/board/board/controller/BoardController.java
+++ src/main/java/com/example/board/board/controller/BoardController.java
@@ -1,8 +1,13 @@
 package com.example.board.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+
 import com.example.board.repository.PostRepository;
+import com.example.board.board.repository.PostFactory;
 import com.example.board.repository.Post;
+import java.util.Optional;
 
 public class BoardController {
 
@@ -37,13 +42,65 @@ public class BoardController {
      * @return テンプレート
      */
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String create(@ModelAttribute("form") Post form, BindingResult result,
-            Model model) {
+    public String create(@ModelAttribute("form") @Validated Post form, BindingResult result, Model model) {
         if (!result.hasErrors()) {
             repository.saveAndFlush(PostFactory.createPost(form));
             model.addAttribute("form", PostFactory.newPost());
         }
         model = this.setList(model);
+        model.addAttribute("path", "create");
+        return "layout";
+    }
+
+    /**
+     * 編集する投稿を表示する
+     *
+     * @param form  フォーム
+     * @param model モデル
+     * @return テンプレート
+     */
+    @RequestMapping(value = "/edit", method = RequestMethod.GET)
+    public String edit(@ModelAttribute("form") Post form, Model model) {
+        Optional<Post> post = repository.findById(form.getId());
+        model.addAttribute("form", post);
+        model = setList(model);
+        model.addAttribute("path", "update");
+        return "layout";
+    }
+
+    /**
+     * 更新する
+     *
+     * @param form  フォーム
+     * @param model モデル
+     * @return テンプレート
+     */
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String update(@ModelAttribute("form") @Validated Post form, BindingResult result, Model model) {
+        if (!result.hasErrors()) {
+            Optional<Post> post = repository.findById(form.getId());
+            repository.saveAndFlush(PostFactory.updatePost(post.get(), form));
+        }
+
+        model.addAttribute("form", PostFactory.newPost());
+        model = setList(model);
+        model.addAttribute("path", "create");
+        return "layout";
+    }
+
+    /**
+     * 削除する
+     *
+     * @param form  フォーム
+     * @param model モデル
+     * @return テンプレート
+     */
+    @RequestMapping(value = "/delete", method = RequestMethod.GET)
+    public String delete(@ModelAttribute("form") Post form, Model model) {
+        Optional<Post> post = repository.findById(form.getId());
+        repository.saveAndFlush(PostFactory.deletePost(post.get()));
+        model.addAttribute("form", PostFactory.newPost());
+        model = setList(model);
         model.addAttribute("path", "create");
         return "layout";
     }
--- src/main/java/com/example/board/board/repository/PostRepository.java
+++ src/main/java/com/example/board/board/repository/PostRepository.java
@@ -2,10 +2,13 @@ package com.example.board.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 /**
  * 投稿のリポジトリー.
  */
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    public Optional<Post> findById(String id);
+
 }
