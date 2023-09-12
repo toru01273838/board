9d872e6a51c959d2ba2c6a3d7961ee531b146416
toru <01273838honda@gmail.com>
2023-09-12 21:30:00 +0900

    commit

--- src/main/java/com/example/board/board/controller/BoardController.java
+++ src/main/java/com/example/board/board/controller/BoardController.java
@@ -1,27 +1,50 @@
 package com.example.board.board.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.board.repository.PostRepository;
+import com.example.board.repository.Post;
 
-import com.example.board.repository.PostFactory;
-
-/**
- * 掲示板のフロントコントローラー.
- */
-@Controller
 public class BoardController {
 
+    /** 投稿の一覧 */
+    @Autowired
+    private PostRepository repository;
+
+    public String index(Model model) {
+        model.addAttribute("form", PostFactory.newPost());
+        model = this.setList(model);
+        model.addAttribute("path", "create");
+        return "layout";
+    }
+
     /**
-     * 一覧を表示する。
+     * 一覧を設定する。+ *
+     * 
+     * @param model モデル
+     * @return 一覧を設定したモデル
+     */
+    private Model setList(Model model) {
+        Iterable<Post> list = repository.findAll();
+        model.addAttribute("list", list);
+        return model;
+    }
+
+    /**
+     * 登録する。
      *
+     * @param form  フォーム
      * @param model モデル
      * @return テンプレート
      */
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Model model) {
-        model.addAttribute("form", PostFactory.newPost());
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String create(@ModelAttribute("form") Post form, BindingResult result,
+            Model model) {
+        if (!result.hasErrors()) {
+            repository.saveAndFlush(PostFactory.createPost(form));
+            model.addAttribute("form", PostFactory.newPost());
+        }
+        model = this.setList(model);
+        model.addAttribute("path", "create");
         return "layout";
     }
 }
